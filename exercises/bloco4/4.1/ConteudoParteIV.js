ccdb1f5570e633238effc4d99614359fb26fc389
Carlos Lima <carlos.augustolimaf@gmail.com>
2021-05-19 19:49:38 -0300

    Exercicio Conteudo até Parte V

---
+++ exercises/bloco4/4.1/ConteudoParteIV.js
@@ -0,0 +1,11 @@
+let notaDesafio = 50;
+
+if (notaDesafio >= 80){
+  console.log("Parabéns, você foi aprovado!");
+} 
+else if (notaDesafio >= 60 && trybe < 80){
+  console.log("Você está na nossa lista de espera!");
+} 
+else {
+  console.log("Você foi reprovado!");
+}
---
+++ exercises/bloco4/4.1/ConteudoParteV.js
@@ -0,0 +1,20 @@
+let estadoPessoa = "lista";
+
+switch (estadoPessoa){
+  case "aprovada":
+    console.log("Parabéns, você foi aprovado!");
+    break;
+
+  case "lista":
+    console.log("Você está na nossa lista de espera!");
+    break;
+
+  case "reprovada":
+    console.log("Você foi reprovada!");
+    break;
+  
+  default:
+    console.log("Não se aplica!");
+
+
+}
