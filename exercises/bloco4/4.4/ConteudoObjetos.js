35bd8df3c57cc2dec247f8610d416575f517a4bb
Carlos Lima <carlos.augustolimaf@gmail.com>
2021-05-24 21:07:15 -0300

    Adiciona Conteudos até o Exercicio parteII

---
+++ exercises/bloco4/4.4/ConteudoObjetos.js
@@ -0,0 +1,19 @@
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { 
+    golden: 2, 
+    silver: 3 
+  }
+}
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
+
+player['medals'] = '2 medalhas de ouro e 3 medalhas de prata'
+
+
